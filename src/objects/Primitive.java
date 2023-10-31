c98bac1a32aaf971e3dd56855320bcc460c47300
Justin-Chua <justin.chua@ucalgary.ca>
2023-10-31 14:18:20 -0600

    Added boilerplate classes for object types and GUI

---
+++ src/objects/Primitive.java
@@ -0,0 +1,16 @@
+package objects;
+
+public class Primitive {
+    
+    private int numeric;
+    private float decimal;
+    private String message;
+    private boolean condition;
+
+    public Primitive(int numeric, float decimal, String message, boolean condition) {
+        this.numeric = numeric;
+        this.decimal = decimal;
+        this.message = message;
+        this.condition = condition;
+    }
+}
---
+++ src/objects/application/TextGUI.java
@@ -0,0 +1,9 @@
+package objects.application;
+
+public class TextGUI {
+    
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
